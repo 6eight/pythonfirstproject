c102d0710090bb64251baa2319099438e0827731
6eight <159606031+6eight@users.noreply.github.com>
2024-02-26 13:25:32 +0100

    Add files via upload

---
+++ first python.py
@@ -0,0 +1,4 @@
+Name =input('enter your Name:')
+Age =input('enter your Age:')
+Location =input('enter your Location:')
+print(f'Hello{Name}, you are how many {Age} years old and live in {Location}.')
